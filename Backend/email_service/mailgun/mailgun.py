fbffe4fdbd233ff40c1054ae52218cf8b1d537c7
your-name <your-email-address>
2022-11-23 15:01:37 +0300

    rabbitmq

---
+++ Backend/email_service/mailgun/mailgun.py
@@ -0,0 +1,14 @@
+import requests
+
+def send_simple_message():
+	return requests.post(
+		"https://api.mailgun.net/v3/sandboxa6ad4b8ad01d4540bc9e059ae1b76a86.mailgun.org/messages",
+		auth=("api", "de254d027a90c9b0d189861101dc35af-69210cfc-e8e20f0e"),
+		data={"from": "sandboxa6ad4b8ad01d4540bc9e059ae1b76a86.mailgun.org",
+			"to": ["katongole.roy100@gmail.com"],
+			"subject": "Hello",
+			"text": "Testing some Mailgun awesomness!"})
+
+x = send_simple_message()
+
+print(x)
